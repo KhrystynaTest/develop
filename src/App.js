69e8c1eea0c772cabbc0fed5d23492481082485f
romankryven <romankryven@gmail.com>
2022-09-28 20:38:40 +0300

    add key to list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Title from "./components/Title";
+import Header from "./components/Header";
+import List from "./components/List";
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [list, setList] = useState([]);
   const listLength = list.length;
 
@@ -11,40 +14,37 @@ function App() {
   };
 
   const addItem = () => {
+    // if (inputValue.trim() === '') return
+
     setList([...list, inputValue]);
-    setInputValue('');
+    setInputValue("");
   };
 
   const deleteItem = (el) => {
-    setList(list.filter(item => item !== el))
+    setList(list.filter((item) => item !== el));
   };
 
   return (
-    <div className={'App'}>
-      <header>
-        <p>Title</p>
-      </header>
+    <div className={"App"}>
+      <Header />
       <main>
-        <div className={'list'}>
-          <h1>List</h1>
+        <div className={"list"}>
+          <Title title={"List"} />
+          <Title title={"Some text"} />
 
           <span>Added {listLength} items</span>
 
-          <div className={'createSection'}>
-            <input onChange={changeInput} value={inputValue} placeholder='Add Item' />
-            <button onClick={addItem}>Add</button>
+          <div className={"createSection"}>
+            <input
+              onChange={changeInput}
+              value={inputValue}
+              placeholder="Add Item"
+            />
+            <button onClick={addItem} disabled={inputValue.trim() === ""}>
+              Add
+            </button>
           </div>
-
-          <ul>
-            {list.map((item, index) => {
-              return (
-                <li key={index}>
-                  <p>{item}</p>
-                  <p onClick={() => deleteItem(item)} className={'deleteButton'}>Delete</p>
-                </li>
-              );
-            })}
-          </ul>
+          <List list={list} handleDelete={deleteItem} />
         </div>
       </main>
     </div>
---
+++ src/components/List.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import "../App.css";
+
+const List = ({ list, handleDelete }) => {
+  return (
+    <ul>
+      {list.map((item, index) => {
+        return (
+          <li key={index}>
+            <p>{item}</p>
+            <p onClick={() => handleDelete(item)} className={"deleteButton"}>
+              Delete
+            </p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+export default List;
